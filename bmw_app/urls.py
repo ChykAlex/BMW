81adc0dd27326ca1b3ab0730bd72e9ed4b616b2c
ChykAlex <asutugin0806@gmail.com>
2024-02-20 01:59:13 +0600

    ura 2

--- bmw_app/urls.py
+++ bmw_app/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('',views.home_page,name = 'home_page'),
     path('news/',views.news_page, name = 'news_page'),
     path('news/detail/<int:pk>/',views.news_detail_page, name = 'news_detail_page'),
-    path('about/<int:pk>/',views.about_page, name = 'about_page'),
 ]
